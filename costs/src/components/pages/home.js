70cbcafd89658f97a203399f48eb895d848eca06
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-12-28 00:58:09 -0500

    home-css

--- costs/src/components/pages/home.js
+++ costs/src/components/pages/home.js
@@ -1,5 +1,16 @@
+import styles from './home.module.css'
+import savings from '../../img/savings.svg'
+
 function Home() {
-    return <h1>Home</h1>
+    return (
+        <section>
+            <h1>Welcome to <span>Costs</span></h1>
+            <p>Start managing your projects</p>
+            <a href="/">Create Projects</a>
+            <img src={savings} alt={Costs} />
+
+        </section>
+    )
 }
 
 export default Home
