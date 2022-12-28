d184959800b2ca3a19898e5e1c4c31fef78bd37f
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-12-27 23:42:09 -0500

    changes

--- costs/src/App.js
+++ costs/src/App.js
@@ -29,6 +29,7 @@ function App() {
           </Route>
         </Container>
       </Switch>
+      <Footer />
     </Router>
   )
 }
---
+++ costs/src/components/pages/layout/Navbar.js
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom'
+
+import Container from './Container'
+
+import styles from '.Navbar.module.css'
+import logo from '../../../img/favicon.ico'
+
+function Navbar() {
+    return(
+        <nav>
+            <Container> 
+                <Link to="/">
+                    <img src={logo} alt="costs"/>
+                </Link>    
+                <Link to="/">Home</Link>
+                <Link to="/contact">Contact</Link>
+                <Link to="/company">Company</Link>
+                <Link to="/newproject">NewProject</Link>
+            </Container>
+        </nav>
+    )
+}
+
+export default Navbar
