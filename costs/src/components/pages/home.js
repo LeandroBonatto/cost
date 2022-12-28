de91dffd5e8c708a0225989b5f70dd9a7ce8561c
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-12-28 01:02:55 -0500

    csshome

--- costs/src/components/pages/home.js
+++ costs/src/components/pages/home.js
@@ -3,7 +3,7 @@ import savings from '../../img/savings.svg'
 
 function Home() {
     return (
-        <section>
+        <section className={styles.home_container}>
             <h1>Welcome to <span>Costs</span></h1>
             <p>Start managing your projects</p>
             <a href="/">Create Projects</a>
