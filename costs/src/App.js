e12f60eb2e887797bbaa40248dd0cf1f6909a846
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-12-27 21:28:00 -0500

    newproject

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,4 +1,9 @@
 import { BrowserRouter as Router, Switch, Route } from 'react=router-dom'
+import Home from './components/pages/Home'
+import Contact from './components/pages/Contact'
+import Company from './components/pages/Company'
+import NewProject from './components/pages/NewProject'
+
 
 function App() {
   return (
