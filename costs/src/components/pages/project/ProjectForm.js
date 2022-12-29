a6735a2fee6a0c56cd9cd9e5502a79c878a4eba0
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-12-28 23:40:38 -0500

    select

--- costs/src/components/pages/project/ProjectForm.js
+++ costs/src/components/pages/project/ProjectForm.js
@@ -1,14 +1,22 @@
+import Input from '../form/input'
+
 import styles from './ProjectForm.module.css'
 
 function ProjectForm() {
     return (
         <form className={styles.form}>
-            <div>
-                <input type="text" placeholder="Insert project name" />
-            </div>
-            <div>
-                <input type="number" placeholder="Insert budget" />
-            </div>
+            <Input 
+                type="text" 
+                text="Project name"
+                name="name"
+                placeholder="Insert project name"
+            />
+            <Input 
+                type="number" 
+                text="project budget"
+                name="budget"
+                placeholder="Insert project budget"
+            />
             <div>
                 <select name="category_id">
                     <option disabled>Select category</option>
