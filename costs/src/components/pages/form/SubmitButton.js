6faf3444a8ad3d01c074c3c5e9008f3138c9dfd4
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-12-29 00:06:01 -0500

    submitbtn

---
+++ costs/src/components/pages/form/SubmitButton.js
@@ -0,0 +1,11 @@
+import styles from './SubmitButton.module.css'
+
+function SubmitButton({ text }) {
+    return (
+        <div>
+           <button className={StyleSheet.btn}></button> 
+        </div>
+    )
+}
+
+export default SubmitButton
