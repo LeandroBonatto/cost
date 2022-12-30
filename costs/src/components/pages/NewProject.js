0100b1bc87ab70d283a525782e0cf656b4723e9d
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-12-29 21:02:39 -0500

    Update NewProject.js

--- costs/src/components/pages/NewProject.js
+++ costs/src/components/pages/NewProject.js
@@ -1,7 +1,28 @@
+import { useHistory } from 'react-router-dom'
 import styles from './NewProject.module.css'
 import ProjectForm from './project/ProjectForm'
 
 function NewProject() {
+
+    const history = useHistory()
+
+    function createPost(project) {
+
+        //Inicialize cost and services
+        project.cost = 0
+        project.services = []
+
+        fetch('http:/localhost:5000/projects', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            }
+        }).then(resp) => resp.json())
+        .then((data) => {})
+        ).catch(err => console.log(err))
+
+    }
+
     return (
     <div className={styles.newproject_container}>
         <h1>Create Project</h1>
